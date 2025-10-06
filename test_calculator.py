fa764b8d974b14d185173138cd53bad15aeaa616
Diya Prakash <pes2ug23cs184@pesu.pes.edu>
2025-10-06 23:03:31 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,5 @@
-from calculator import add, subtract, multiply, divide
+from calculator import add, subtract, multiply, divide, power, square_root
+import pytest
 
 def test_add():
     assert add(2, 3) == 5
